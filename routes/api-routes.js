8bbba0d1ea9586f01bcbd9d1b777c9719e7a80d1
Caleb Parris <calebparris@live.com>
2020-12-21 07:37:29 -0600

    Tweaked the delete requests

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -101,7 +101,7 @@ module.exports = function (app) {
   });
 
   app.delete("/api/medications/:_id", (req, res) => {
-    db.Medication.findOneAndDelete(req.params._id)
+    db.Medication.findOneAndDelete({ _id: req.params._id })
       .then((medData) => {
         res.json(medData);
       })
@@ -111,7 +111,7 @@ module.exports = function (app) {
   });
 
   app.delete("/api/appointments/:_id", (req, res) => {
-    db.Appointment.findOneAndDelete(req.params._id)
+    db.Appointment.findOneAndDelete({ _id: req.params._id })
       .then((appointData) => {
         res.json(appointData);
       })
